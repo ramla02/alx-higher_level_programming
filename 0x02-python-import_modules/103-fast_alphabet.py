8a62ab4963bea87527e568334728e1b8803fecb9
ramla02 <ramla252017@gmail.com>
2024-06-15 19:10:22 +0300

    Everything can be imported

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-"""
-Write a program that prints the alphabet in uppercase, followed by a new line.
-Your program should be maximum 3 lines long
-"""
 import string
 print(string.ascii_uppercase)
