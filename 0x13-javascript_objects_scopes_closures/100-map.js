254ba92092814ed44a5b2b9e1d6ec726ac12accb
ramla02 <ramla252017@gmail.com>
2024-06-18 13:44:42 +0300

    Factor index

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/**
+* Write a script that imports an array and computes a new array.
+*/
+
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
